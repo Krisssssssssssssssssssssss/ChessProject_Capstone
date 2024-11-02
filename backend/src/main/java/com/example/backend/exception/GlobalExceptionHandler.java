437457136aa0dc72547e2038d38ec64bb751640f
kristijanjakimov <kiko.jakimov@gmail.com>
2024-11-02 18:48:55 +0100

    exception handler added

---
+++ backend/src/main/java/com/example/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.example.backend.exception;
+import com.example.backend.dto.ErrorResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+@Log4j2
+public class GlobalExceptionHandler {
+    private static final String GENERIC_ERROR_MESSAGE = "Something went wrong";
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleGlobalException(NoSuchElementException exception) {
+        log.info(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleGlobalException(HttpMessageNotReadableException exception) {
+        log.info(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleGlobalException(NoResourceFoundException exception) {
+        log.info(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(HandlerMethodValidationException exception) {
+        log.info(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleValidationException(ResponseStatusException exception) {
+        log.info(exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+        log.info("Data integrity violation: " + exception.getMessage());
+        return new ErrorResponse("Data integrity violation: duplicate entry or constraint violation.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGlobalException(Exception exception) {
+        log.error(exception);
+        return new ErrorResponse(GENERIC_ERROR_MESSAGE);
+    }
+}
