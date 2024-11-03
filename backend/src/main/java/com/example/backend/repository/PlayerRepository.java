c81e3ab3b5f056b3107354bbddf344218090b019
kristijanjakimov <kiko.jakimov@gmail.com>
2024-11-03 16:04:20 +0100

    Sonar added

--- backend/src/main/java/com/example/backend/repository/PlayerRepository.java
+++ backend/src/main/java/com/example/backend/repository/PlayerRepository.java
@@ -1,5 +1,6 @@
-package com.example.backend.model;
+package com.example.backend.repository;
 
+import com.example.backend.model.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
