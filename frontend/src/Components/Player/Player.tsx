7e158e2571514cd1c1160c0a11428117b50ea3b9
kristijanjakimov <kiko.jakimov@gmail.com>
2024-11-14 16:57:01 +0100

    front end done

--- frontend/src/Components/Player/Player.tsx
+++ frontend/src/Components/Player/Player.tsx
@@ -1,24 +1,42 @@
 import UserResponse from "../../Types/UserResponse.ts";
-import Chessboard from "../Chessboard.tsx";
+import {Chessboard} from "react-chessboard";
+import {useState, useEffect} from "react";
 
 interface PlayerProps {
     currentUserName: string;
     selectedUser: UserResponse | null;
 }
 
-export default function Player({ currentUserName, selectedUser }: PlayerProps) {
+export default function Player({currentUserName, selectedUser}: PlayerProps) {
+    const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    function onDrop(sourceSquare: any, targetSquare: any) {
+        console.log(sourceSquare);
+        console.log(targetSquare);
+    }
+
+    useEffect(() => {
+        if (selectedUser) {
+            setFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        }
+    }, [selectedUser]);
+
     return (
         <div className="main_inner">
-            {selectedUser ? (
+            {selectedUser && (
                 <div className="top-left-player">
-                    <i className="fa-solid fa-user fa-2xl" style={{ color: "#000000" }}></i> {selectedUser.name}
+                    <i className="fa-solid fa-user fa-2xl" style={{color: "#000000"}}></i> {selectedUser.name}
                 </div>
-            ) : (
-                <p className="select-player-text">Select a player you want to play against!</p>
             )}
 
             <div className="chessboard-container">
-                <Chessboard />
+                {selectedUser ? (
+                    <div className="chessboard-container">
+                        <Chessboard position={fen} onPieceDrop={onDrop}/>
+                    </div>
+                ) : (
+                    <p className="select-player-text">Select a player you want to play against!</p>
+                )}
             </div>
 
             {selectedUser && (
