35e18c34c314b7b888d2d548589561e48d35ea21
kristijanjakimov <kiko.jakimov@gmail.com>
2024-11-16 00:01:17 +0100

    Unit and Integration tests added

--- backend/src/main/java/com/example/backend/model/Tile.java
+++ backend/src/main/java/com/example/backend/model/Tile.java
@@ -1,18 +1,18 @@
-package com.example.backend.model;
-
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@With
-@Data
-public class Tile {
-    String name;
-    int id;
-    int x;
-    int y;
-    boolean isOccupied;
-    String color;
-    String piece;
-}
+//package com.example.backend.model;
+//
+//import lombok.*;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@With
+//@Data
+//public class Tile {
+//    String name;
+//    int id;
+//    int x;
+//    int y;
+//    boolean isOccupied;
+//    String color;
+//    String piece;
+//}
