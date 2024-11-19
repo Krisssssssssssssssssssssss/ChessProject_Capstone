b7453889e5a0f48918a2c0d8d56d94f38da69701
kristijanjakimov <kiko.jakimov@gmail.com>
2024-11-19 03:05:13 +0100

    Sliding pieces be slidin'

--- backend/src/main/java/com/example/backend/service/pieceMovement/SlidingPiecesService.java
+++ backend/src/main/java/com/example/backend/service/pieceMovement/SlidingPiecesService.java
@@ -1,6 +1,5 @@
 package com.example.backend.service.pieceMovement;
 
-import com.example.backend.model.GameModel;
 import com.example.backend.model.Pieces.*;
 import com.example.backend.model.Tile;
 
@@ -45,7 +44,15 @@ public class SlidingPiecesService {
         if ((sourceTile.getY() != targetTile.getY() && (sourceTile.getX() != targetTile.getX()))) {
             return false;
         }
-        return true;
+        int xSum = howManyFieldsStraightLine(sourceTile.getX(), targetTile.getX());
+        int ySum = howManyFieldsStraightLine(sourceTile.getY(), targetTile.getY());
+        int xySumBigger;
+        if (xSum > ySum) {
+            xySumBigger = xSum;
+        }
+        else {xySumBigger = ySum;}
+        boolean isJumpingOver = isJumpingOverStraightLine(board, sourceTile, targetTile, xySumBigger);
+        return isJumpingOver;
     }
 
     private static boolean isRestrictedBishop(List<List<Tile>> board, Tile sourceTile, Tile targetTile, Piece pieceToMove) {
@@ -54,14 +61,15 @@ public class SlidingPiecesService {
         if (xSum != ySum) {
             return false;
         }
-        return true;
+        boolean isJumpingOver = isJumpingOverDiagonally(board, sourceTile, targetTile, xSum);
+        return isJumpingOver;
     }
 
     private static boolean isRestrictedQueen(List<List<Tile>> board, Tile sourceTile, Tile targetTile, Piece pieceToMove) {
-        if (isRestrictedRook(board, sourceTile, targetTile, pieceToMove) || isRestrictedBishop(board, sourceTile, targetTile, pieceToMove)) {
-            return true;
+        if (!isRestrictedRook(board, sourceTile, targetTile, pieceToMove) && !isRestrictedBishop(board, sourceTile, targetTile, pieceToMove)) {
+            return false;
         }
-        return false;
+        return true;
     }
 
     private static int howManyFieldsDiagonally(int starting, int ending) {
@@ -71,5 +79,155 @@ public class SlidingPiecesService {
             return ending - starting;
         }
     }
+    private static int howManyFieldsStraightLine(int starting, int ending) {
+        if (starting > ending) {
+            return starting - ending;
+        } else {
+            return ending - starting;
+        }
+    }
 
+    private static boolean isJumpingOverStraightLine(List<List<Tile>> board, Tile sourceTile, Tile targetTile, int xySum) {
+        boolean isXchanging = false;
+        boolean isXincreasing = false;
+        boolean isYincreasing = false;
+
+        if (sourceTile.getX() == targetTile.getX()) {
+            if (sourceTile.getY() < targetTile.getY()) {
+                isYincreasing = true;
+            }
+        } else {
+            isXchanging = true;
+            if (sourceTile.getX() < targetTile.getX()) {
+                isXincreasing = true;
+            }
+        }
+
+        //We always calculate from the source
+        if (isXchanging) {
+            if (isXincreasing) {
+                for (int i = 1; i < xySum; i++) {
+                    for (List<Tile> row : board) {
+                        for (Tile tile : row) {
+                            if (tile.getX() == sourceTile.getX() + i && tile.getY() == sourceTile.getY()) {
+                                if (tile.isOccupied()) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!isXincreasing) {
+                for (int i = 1; i < xySum; i++) {
+                    for (List<Tile> row : board) {
+                        for (Tile tile : row) {
+                            if (tile.getX() == sourceTile.getX() - i && tile.getY() == sourceTile.getY()) {
+                                if (tile.isOccupied()) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if (isYincreasing) {
+                for (int i = 1; i < xySum; i++) {
+                    for (List<Tile> row : board) {
+                        for (Tile tile : row) {
+                            if (tile.getY() == sourceTile.getY() + i && tile.getX() == sourceTile.getX()) {
+                                if (tile.isOccupied()) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!isYincreasing) {
+                for (int i = 1; i < xySum; i++) {
+                    for (List<Tile> row : board) {
+                        for (Tile tile : row) {
+                            if (tile.getY() == sourceTile.getY() - i && tile.getX() == sourceTile.getX()) {
+                                if (tile.isOccupied()) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    private static boolean isJumpingOverDiagonally(List<List<Tile>> board, Tile sourceTile, Tile targetTile, int xySum) {
+        boolean isXincreasing;
+        boolean isYincreasing;
+        if (sourceTile.getX() < targetTile.getX()) {
+            isXincreasing = true;
+        } else {
+            isXincreasing = false;
+        }
+        if (sourceTile.getY() < targetTile.getY()) {
+            isYincreasing = true;
+        } else {
+            isYincreasing = false;
+        }
+        //We always calculate from the source
+        if (isXincreasing && isYincreasing) {
+            for (int i = 1; i < xySum; i++) {
+                for (List<Tile> row : board) {
+                    for (Tile tile : row) {
+                        if (tile.getX() == sourceTile.getX() + i && tile.getY() == sourceTile.getY() + i) {
+                            if (tile.isOccupied()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!isXincreasing && !isYincreasing) {
+            for (int i = 1; i < xySum; i++) {
+                for (List<Tile> row : board) {
+                    for (Tile tile : row) {
+                        if (tile.getX() == sourceTile.getX() - i && tile.getY() == sourceTile.getY() - i) {
+                            if (tile.isOccupied()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (isXincreasing && !isYincreasing) {
+            for (int i = 1; i < xySum; i++) {
+                for (List<Tile> row : board) {
+                    for (Tile tile : row) {
+                        if (tile.getX() == sourceTile.getX() + i && tile.getY() == sourceTile.getY() - i) {
+                            if (tile.isOccupied()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!isXincreasing && isYincreasing) {
+            for (int i = 1; i < xySum; i++) {
+                for (List<Tile> row : board) {
+                    for (Tile tile : row) {
+                        if (tile.getX() == sourceTile.getX() - i && tile.getY() == sourceTile.getY() + i) {
+                            if (tile.isOccupied()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
